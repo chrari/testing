2084ed8775958fd3bb32613cf66b0360587e804f
chrari <chm.a.2k@gmail.com>
2020-10-11 18:17:06 +0200

    Revert "One more"
    This reverts commit 5eaa7389b0d45eb93997b8c3de53d64191cdb03f.

--- febrer.php
+++ febrer.php
@@ -1,4 +1,3 @@
 echo 'Pel febrer, carnestoltes el primer';
 echo 'Pel febrer flor a l'ametller';
 echo 'Més tard o més matiner carnaval dins el febrer';
-echo 'Has de llaurar al Febrer el que al marc vulgues sembrar';
